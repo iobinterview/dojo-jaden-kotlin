af5bcc5a115b32c439e5f51499496a63f5c017ff
Marcel Bochtler <marcel.bochtler@bosch.io>
2022-08-30 16:37:04 +0200

    Initial commit

---
+++ src/test/kotlin/JadenCaseTest.kt
@@ -0,0 +1,30 @@
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
+class JadenCaseTest {
+    private val jadenCase = JadenCase()
+
+    @Test
+    fun casingTest1() {
+        assertEquals("Most Trees Are Blue", jadenCase.toJadenCase("most trees are blue"))
+    }
+
+    @Test
+    fun casingTest2() {
+        assertEquals(
+            "How Can Mirrors Be real If Our Eyes Aren't Real",
+            jadenCase.toJadenCase("How can mirrors be real if our eyes aren't real")
+        )
+    }
+
+    @Test
+    fun testNullArg() {
+        Assertions.assertNull(jadenCase.toJadenCase(null))
+    }
+
+    @Test
+    fun testEmptyArg() {
+        Assertions.assertNull(jadenCase.toJadenCase(""))
+    }
+}
